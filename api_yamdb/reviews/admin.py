f2553f23495bcb686199b139f9881777827043de
Павел Перевозкин <pavel.perevozkin@gmail.com>
2022-09-26 00:15:45 +0300

    yamdb_final_review_1

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-
-from .models import Category, Comments, Genres, Review, Title 
 from users.models import User
+
+from .models import Category, Comments, Genres, Review, Title
 
 admin.site.register(Category)
 admin.site.register(Genres)
